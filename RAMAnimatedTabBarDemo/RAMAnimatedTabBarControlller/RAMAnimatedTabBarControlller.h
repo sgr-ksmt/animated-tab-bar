e2564a29903f147669a0db6cd0c188db38fbd863
sgr-ksmt <melodydance.k.s@gmail.com>
2016-03-09 13:15:00 +0900

    Add Carthage support

---
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarControlller/RAMAnimatedTabBarControlller.h
@@ -0,0 +1,19 @@
+//
+//  RAMAnimatedTabBarControlller.h
+//  RAMAnimatedTabBarControlller
+//
+//  Created by Suguru Kishimoto on 2016/03/09.
+//  Copyright © 2016年 Ramotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RAMAnimatedTabBarControlller.
+FOUNDATION_EXPORT double RAMAnimatedTabBarControlllerVersionNumber;
+
+//! Project version string for RAMAnimatedTabBarControlller.
+FOUNDATION_EXPORT const unsigned char RAMAnimatedTabBarControlllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAMAnimatedTabBarControlller/PublicHeader.h>
+
+
